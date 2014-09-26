fea383dafd09f3d44d338c0c2ef4a7279de8d6d7
Lucas Romao Silva <luck.romao@gmail.com>
2014-09-25 21:58:39 -0300

    Finalizada geração do arquivo de saida

--- miniEp03/dnsConversor/dnsReverse.py
+++ miniEp03/dnsConversor/dnsReverse.py
@@ -17,13 +17,17 @@ class Conversor():
     exp3 = re.compile("\s*(\d+|\d+[MHDW])\s*[;)]")
     exp4 = re.compile("\s*(IN)?\s*NS\s+\w")
     exp5 = re.compile("\s*\w+\s+IN\s+A\s+(\d+[.]?)+")
-    fqdn = ""
 
     def __init__(self, arquivoEntrada, arquivoSaida="saida"):
-        sys.stdin = open(arquivoEntrada, "r")
-        sys.stdout = open(arquivoSaida, "w")
+        try:
+            sys.stdin = open(arquivoEntrada, "r")
+            sys.stdout = open(arquivoSaida, "w")
+        except FileNotFoundError:
+            sys.exit("Erro na leitura do arquivo!")
+
 
     def converte(self):
+        fqdn = ""
         for line in sys.stdin:
             if self.exp1.match(line) != None or self.exp2.match(line) != None \
                     or self.exp3.match(line) != None:
@@ -32,4 +36,10 @@ class Conversor():
                 print(line)
                 v = line.split()
                 fqdn = v[len(v) - 1].split(".", 1)
+            elif self.exp5.match(line) != None:
+                v = line.split()
+                aux = v[len(v) - 1].split(".")
+                txt = "" + aux[len(aux) - 1] + "    IN PTR"
+                txt += "    " + v[0] + "." + fqdn[1] + "\n"
+                print(txt)
 
